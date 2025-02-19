5b98a7c0cfe26fa51fdd63c99736e82882ac2726
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2025-02-19 12:40:28 -0500

    Rename project in package.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  env: { browser: true, es2021: true },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   extends: ['airbnb-base', 'prettier'],
+  rules: { 'no-unused-vars': ['error', { argsIgnorePattern: 'next' }] },
+
   overrides: [
     {
-      env: {
-        node: true,
-      },
+      env: { node: true },
       files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+      parserOptions: { sourceType: 'script' },
     },
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-  },
 };
--- app.js
+++ app.js
@@ -1,15 +1,42 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(compression());
 app.use(express.json());
+app.use(helmet());
+
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Page not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  const response = {
+    error: {
+      message: err.message,
+      status: err.status || 500,
+      stack: err.stack,
+    },
+  };
+
+  console.error(response);
+  return res.json(response);
+});
 
 app.listen(PORT, () =>
   console.log(`My first Express app - listening on port ${PORT}!`),
